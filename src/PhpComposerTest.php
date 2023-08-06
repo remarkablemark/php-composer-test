a1f5e7c45722b3abf9b1fa16a1651819ba4c0a9c
Mark <mark@remarkablemark.org>
2023-08-06 02:01:29 -0400

    refactor: run php-cs-fixer

--- src/PhpComposerTest.php
+++ src/PhpComposerTest.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace Remarkablemark\PhpComposerTest;
 
-class PhpComposerTest {
+class PhpComposerTest
+{
     public function say(string $name = 'world'): string
     {
-        return "Hello, $name!";
+        return "Hello, {$name}!";
     }
 }
--- tests/PhpComposerTest.php
+++ tests/PhpComposerTest.php
@@ -1,13 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
 use Remarkablemark\PhpComposerTest\PhpComposerTest;
 
-it('says "Hello, world!"', function () {
+it('says "Hello, world!"', function (): void {
     $test = new PhpComposerTest();
     expect($test->say())->toBe('Hello, world!');
 });
 
-it('says "Hello, Mark!"', function () {
+it('says "Hello, Mark!"', function (): void {
     $test = new \Remarkablemark\PhpComposerTest\PhpComposerTest();
     expect($test->say('Mark'))->toBe('Hello, Mark!');
 });
